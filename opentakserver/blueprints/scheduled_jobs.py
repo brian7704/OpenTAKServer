077beccc5454f567e7718b083e97473cd29616a9
Brian Wallen <bwallen@gmail.com>
2024-02-29 17:55:25 -0500

    Added a cron job that will purge all data except user accounts from the DB

--- opentakserver/blueprints/scheduled_jobs.py
+++ opentakserver/blueprints/scheduled_jobs.py
@@ -10,7 +10,22 @@ from flask import current_app as app
 from opentakserver.extensions import apscheduler, logger, db
 import requests
 
+from opentakserver.models.Chatrooms import Chatroom
+from opentakserver.models.Alert import Alert
+from opentakserver.models.CasEvac import CasEvac
+from opentakserver.models.Certificate import Certificate
+from opentakserver.models.ChatroomsUids import ChatroomsUids
+from opentakserver.models.CoT import CoT
+from opentakserver.models.DataPackage import DataPackage
+from opentakserver.models.EUD import EUD
+from opentakserver.models.GeoChat import GeoChat
+from opentakserver.models.Marker import Marker
+from opentakserver.models.Point import Point
+from opentakserver.models.RBLine import RBLine
+from opentakserver.models.Team import Team
 from opentakserver.models.VideoRecording import VideoRecording
+from opentakserver.models.VideoStream import VideoStream
+from opentakserver.models.ZMIST import ZMIST
 
 scheduler_blueprint = Blueprint('scheduler_blueprint', __name__)
 
@@ -61,8 +76,32 @@ def delete_video_recordings():
                 recordings = r.json()
                 for path in recordings['items']:
                     for recording in path['segments']:
-                        r = requests.delete('http://localhost:9997/v3/recordings/deletesegment', params={'start': recording['start'], 'path': path['name']})
+                        r = requests.delete('http://localhost:9997/v3/recordings/deletesegment',
+                                            params={'start': recording['start'], 'path': path['name']})
                         if r.status_code != 200:
-                            logger.error("Failed to delete {} from {}: {}".format(recording['start'], path['name'], r.text))
+                            logger.error(
+                                "Failed to delete {} from {}: {}".format(recording['start'], path['name'], r.text))
         except BaseException as e:
             logger.error("Failed to delete recordings: {}".format(e))
+
+
+@apscheduler.task("cron", id="purge_data", name="Purge Data", day="*", hour=0, minute=0, next_run_time=None)
+def purge_data():
+    delete_video_recordings()
+    Alert.query.delete()
+    CasEvac.query.delete()
+    Certificate.query.delete()
+    Chatroom.query.delete()
+    ChatroomsUids.query.delete()
+    CoT.query.delete()
+    DataPackage.query.delete()
+    EUD.query.delete()
+    GeoChat.query.delete()
+    Marker.query.delete()
+    Point.query.delete()
+    RBLine.query.delete()
+    Team.query.delete()
+    VideoStream.query.delete()
+    ZMIST.query.delete()
+    db.session.commit()
+    logger.info("Purged all data")
