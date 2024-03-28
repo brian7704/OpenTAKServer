154663e1b58cd9f357860f4c4484ab444c3a3192
Brian Wallen <bwallen@gmail.com>
2024-03-28 11:26:45 -0400

    Added lastversion to the dependencies for the installer

--- opentakserver/__init__.py
+++ opentakserver/__init__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.0.0-post.122+ac6b3f8.dirty"
-__version_tuple__ = (0, 0, 0, "post", 122, "ac6b3f8.dirty")
+__version__ = "0.0.0-post.125+06a1f83.dirty"
+__version_tuple__ = (0, 0, 0, "post", 125, "06a1f83.dirty")
