1a78feb3e75484959b532aa8259a4a190e757ae5
Brian Wallen <bwallen@gmail.com>
2024-10-04 11:35:25 -0400

    Version bump in __init__.py

--- opentakserver/__init__.py
+++ opentakserver/__init__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "1.3.0-post.15+c9ad902"
-__version_tuple__ = (1, 3, 0, "post", 15, "c9ad902")
+__version__ = "1.3.0"
+__version_tuple__ = (1, 3, 0)
