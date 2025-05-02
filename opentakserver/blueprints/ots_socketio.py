7b1ffc381eca9ca953f02d205cd06f87c3064e04
Brian Wallen <bwallen@gmail.com>
2025-05-02 13:13:30 -0400

    Added administrator_only decorator for socket.io events

--- opentakserver/blueprints/ots_socketio.py
+++ opentakserver/blueprints/ots_socketio.py
@@ -21,6 +21,18 @@ def authenticated_only(f):
     return wrapped
 
 
+def administrator_only(f):
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        if not current_user.is_authenticated and not current_user.has_role('administrator'):
+            logger.debug("Disconnecting {} from {}".format(request.sid, request.namespace))
+            disconnect(request.sid, namespace=request.namespace)
+        else:
+            return f(*args, **kwargs)
+
+    return wrapped
+
+
 @socketio.on('connect', namespace="/socket.io")
 @authenticated_only
 def connect(data):
@@ -29,5 +41,5 @@ def connect(data):
 
 @socketio.on('message', namespace="/socket.io")
 @authenticated_only
-def message(message):
-    logger.debug("Got a message".format(message))
+def data(data):
+    logger.debug("Got a message".format(data))
