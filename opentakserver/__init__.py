7f3fd9292900dfc9f852a49b4e551d6e1938726a
Brian <bwallen@gmail.com>
2025-07-27 14:58:37 -0400

    Fixed an import bug in /api/data_packages

--- opentakserver/__init__.py
+++ opentakserver/__init__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "1.5.0-post.1+051198a"
-__version_tuple__ = (1, 5, 0, "post", 1, "051198a")
+__version__ = "1.5.0"
+__version_tuple__ = (1, 5, 0)
