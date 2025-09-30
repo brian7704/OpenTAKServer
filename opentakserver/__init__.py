eb189cfbfdc91be8a20e60112967639f564a5469
Brian <bwallen@gmail.com>
2025-09-30 12:36:01 -0400

    Added poetry as a dependency

--- opentakserver/__init__.py
+++ opentakserver/__init__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "1.5.10"
-__version_tuple__ = (1, 5, 10)
+__version__ = "1.5.11"
+__version_tuple__ = (1, 5, 11)
