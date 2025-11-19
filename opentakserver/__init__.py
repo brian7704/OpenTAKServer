0b28970da70dd36bf04838217352ff0a97d50e96
Brian <bwallen@gmail.com>
2025-11-19 08:33:33 -0500

    Bumped dependencies which removed the pkg_resources deprecation warning, closes #181

--- opentakserver/__init__.py
+++ opentakserver/__init__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "1.6.0"
-__version_tuple__ = (1, 6, 0)
+__version__ = "1.6.0-post.14+3b19c08.dirty"
+__version_tuple__ = (1, 6, 0, "post", 14, "3b19c08.dirty")
