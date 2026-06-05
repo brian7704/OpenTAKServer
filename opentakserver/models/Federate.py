bff1ffafb6c904fa9591714a399a3f935e0c98ff
Brian <bwallen@gmail.com>
2026-06-05 10:23:38 -0400

    Fixed federation DB schema

--- opentakserver/models/Federate.py
+++ opentakserver/models/Federate.py
@@ -1,7 +1,7 @@
 from sqlalchemy import JSON, TEXT, DateTime, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-
+from opentakserver.functions import false_values
 from opentakserver.extensions import db
 from opentakserver.functions import iso8601_string_from_datetime
 
@@ -15,21 +15,30 @@ class Federate(db.Model):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(255))
-    shared_alerts: Mapped[bool] = mapped_column(Boolean)
-    archive: Mapped[bool] = mapped_column(Boolean)
-    federate_group_matching: Mapped[bool] = mapped_column(Boolean)
-    automatic_group_matching: Mapped[bool] = mapped_column(Boolean)
-    fallback_group_matching: Mapped[bool] = mapped_column(Boolean)
+    shared_alerts: Mapped[bool] = mapped_column(Boolean, default=True, false_values=false_values)
+    archive: Mapped[bool] = mapped_column(Boolean, default=False, false_values=false_values)
+    federate_group_matching: Mapped[bool] = mapped_column(
+        Boolean, default=True, false_values=false_values
+    )
+    automatic_group_matching: Mapped[bool] = mapped_column(
+        Boolean, default=True, false_values=false_values
+    )
+    fallback_group_matching: Mapped[bool] = mapped_column(
+        Boolean, default=False, false_values=false_values
+    )
     # -1 is takserver's default for no limit
     max_hops: Mapped[int] = mapped_column(Integer, default=-1)
     # When enabled, group hop limiting will be used in place of max hops. Individual group hop limits can be configured on the 'edit groups' page
-    use_group_hop_limiting: Mapped[bool] = mapped_column(Boolean)
+    use_group_hop_limiting: Mapped[bool] = mapped_column(
+        Boolean, default=False, false_values=false_values
+    )
     # takserver notes field says "Notes may contain upper and lower case letters, numbers, spaces and underscores up to 30 characters."
-    notes: Mapped[str] = mapped_column(String(255))
+    notes: Mapped[str] = mapped_column(String(255), nullable=True)
     certificate_file: Mapped[str] = mapped_column(String(255))
     issuer: Mapped[str] = mapped_column(String(255))
     subject: Mapped[str] = mapped_column(String(255))
-    serial_number: Mapped[int] = mapped_column(Integer)
+    serial_number: Mapped[str] = mapped_column(String(255), unique=True)
+    federation_connections = relationship("FederationConnection", back_populates="federate")
 
     def from_wtf(self, form):
         self.name = form.name.data
@@ -43,3 +52,21 @@ class Federate(db.Model):
         self.issuer = form.issuer.data
         self.subject = form.subject.data
         self.serial_number = form.serial_number.data
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "shared_alerts": self.shared_alerts,
+            "federate_group_matching": self.federate_group_matching,
+            "automatic_group_matching": self.automatic_group_matching,
+            "max_hops": self.max_hops,
+            "use_group_hop_limiting": self.use_group_hop_limiting,
+            "notes": self.notes,
+            "certificate_file": self.certificate_file,
+            "issuer": self.issuer,
+            "subject": self.subject,
+            "serial_number": self.serial_number,
+        }
+
+    def to_json(self):
+        return self.serialize()
