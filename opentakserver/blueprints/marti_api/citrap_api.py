1bcee0b44fc9404b943266877f334c739eb9229f
Brian <bwallen@gmail.com>
2026-08-16 14:27:29 -0400

    Added citrap endpoints

--- opentakserver/blueprints/marti_api/citrap_api.py
+++ opentakserver/blueprints/marti_api/citrap_api.py
@@ -6,13 +6,46 @@ from flask import jsonify, request
 citrap_api_blueprint = Blueprint("citrap_api_blueprint", __name__)
 
 
-@citrap_api_blueprint.route("/Marti/api/missions/citrap/subscription", methods=["PUT"])
-def citrap_subscription():
-    uid = bleach.clean(request.args.get("uid"))
-    response = {"version": 3, "type": "com.bbn.marti.sync.model.MissionSubscription", "data": {}}
-    return "", 201
+@citrap_api_blueprint.route("/Marti/api/citrap")
+def search_citrap():
+    keywords = request.args.get("keywords")
+    bbox = request.args.get("bbox")
+    start_time = request.args.get("startTime")
+    end_time = request.args.get("endTime")
+    max_report_count = request.args.get("maxReportCount")
+    report_type = request.args.get("type")
+    callsign = request.args.get("callsign")
+    subscribe = request.args.get("subscribe")
+    client_uid = request.args.get("clientUid")
+    return jsonify([])
 
 
 @citrap_api_blueprint.route("/Marti/api/citrap")
-def citrap():
-    return jsonify([])
+def add_citrap():
+    client_uid = request.args.get("clientUid")
+    return ""
+
+
+@citrap_api_blueprint.route("/Marti/api/citrap/<id>", methods=["GET"])
+def get_citrap(id):
+    client_uid = request.args.get("clientUid")
+    return ""
+
+
+@citrap_api_blueprint.route("/Marti/api/citrap/<id>", methods=["PUT"])
+def put_citrap(id):
+    client_uid = request.args.get("clientUid")
+    return ""
+
+
+@citrap_api_blueprint.route("/Marti/api/citrap/<id>", methods=["DELETE"])
+def delete_citrap(id):
+    client_uid = request.args.get("clientUid")
+    return ""
+
+
+@citrap_api_blueprint.route("/Marti/api/citrap/attachment", methods=["POST"])
+def add_attachment():
+    client_uid = request.args.get("clientUid")
+    # body is JSON
+    return ""
