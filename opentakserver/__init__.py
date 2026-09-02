3c5d337aa9a3226a1c39a582aa83ea474be9b904
Brian <bwallen@gmail.com>
2026-09-02 00:36:45 -0400

    Updated more deps

--- opentakserver/__init__.py
+++ opentakserver/__init__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "1.7.13-post.58+e54956a"
-__version_tuple__ = (1, 7, 13, "post", 58, "e54956a")
+__version__ = "1.7.13-post.59+ae22350"
+__version_tuple__ = (1, 7, 13, "post", 59, "ae22350")
