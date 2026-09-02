49709e804f22d210635dc8cfea1c96a5101f22ca
Brian <bwallen@gmail.com>
2026-09-02 00:53:10 -0400

    Removed eventlet

--- opentakserver/__init__.py
+++ opentakserver/__init__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "1.7.13-post.59+ae22350"
-__version_tuple__ = (1, 7, 13, "post", 59, "ae22350")
+__version__ = "1.7.13-post.60+3c5d337"
+__version_tuple__ = (1, 7, 13, "post", 60, "3c5d337")
